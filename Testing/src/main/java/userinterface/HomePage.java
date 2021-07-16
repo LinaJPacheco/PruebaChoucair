008e320b026b5f6ff04a7f65a0b6e03be26f7cd9
Lina Pacheco <33501210+LinaJPacheco@users.noreply.github.com>
2021-07-16 06:11:31 -0500

    Revert "Prueba de caso de prueba"
    This reverts commit 46dfd2d4b1280c6c4cc5ac9cb2d244e0aa42a99e.

--- Testing/src/main/java/userinterface/HomePage.java
+++
@@ -1,8 +0,0 @@
-package userinterface;
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("https://www.choucairtesting.com/empleos-testing/")
-public class HomePage extends PageObject {
-}
